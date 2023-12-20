6dc116a2912646720589bed93e4cce1f7f469d02
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-12-20 21:44:49 +0200

    init database

--- lib/layouts/home_page.dart
+++ lib/layouts/home_page.dart
@@ -4,6 +4,7 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:memory_bank/views/archived_screen.dart';
 import 'package:memory_bank/views/favourites_screen.dart';
 import 'package:memory_bank/views/memories_screen.dart';
+import 'package:sqflite/sqflite.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,58 +14,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   int _selectedIndex = 0;
-   final List _pages = [
-     {
-       'page' : const MemoriesScreen(),
-       'title' : "Your Memories"
-     },
-     {
-       'page' : const FavouritesScreen(),
-       'title' : "Favourite Memories",
-     },
-     {
-       'page' : const ArchivedScreen(),
-       'title' : "Archived Memories",
-     },
-   ];
+  int _selectedIndex = 0;
+  final List _pages = [
+    {'page': const MemoriesScreen(), 'title': "Your Memories"},
+    {
+      'page': const FavouritesScreen(),
+      'title': "Favourite Memories",
+    },
+    {
+      'page': const ArchivedScreen(),
+      'title': "Archived Memories",
+    },
+  ];
+  @override
+  void initState() {
+    super.initState();
+    createDatabase();
+  }
+
+  late Database database;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_pages[_selectedIndex]["title"],style: GoogleFonts.aDLaMDisplay(),),
+        title: Text(
+          _pages[_selectedIndex]["title"],
+          style: GoogleFonts.aDLaMDisplay(color: Colors.grey),
+        ),
         centerTitle: true,
+        backgroundColor: const Color.fromRGBO(249, 249, 224, 1),
+        //shadowColor: const Color.fromRGBO(255, 192, 217, 1),
+        elevation: 6,
       ),
       body: _pages[_selectedIndex]["page"],
-      floatingActionButton: FloatingActionButton(onPressed: () {},child: const Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          insertToDatabase();
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.grey,
+        ),
+      ),
       bottomNavigationBar: Container(
         decoration: const BoxDecoration(
-          color: Color.fromRGBO(249, 249, 224,1),
+          color: Color.fromRGBO(249, 249, 224, 1),
           boxShadow: <BoxShadow>[
-            BoxShadow(color: Color.fromRGBO(255, 192, 217,1), blurRadius: 9)
+            BoxShadow(color: Color.fromRGBO(255, 192, 217, 1), blurRadius: 9)
           ],
         ),
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: GNav(
-              padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 15),
-              onTabChange: (newIndex)
-              {
-               setState(() {
-                 _selectedIndex = newIndex;
-               });
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              onTabChange: (newIndex) {
+                setState(() {
+                  _selectedIndex = newIndex;
+                });
               },
               gap: 10,
               activeColor: Colors.grey,
-              tabActiveBorder: Border.all(color: const Color.fromRGBO(255, 192, 217,1)),
+              tabActiveBorder:
+                  Border.all(color: const Color.fromRGBO(255, 192, 217, 1)),
               duration: const Duration(milliseconds: 600),
               tabBorderRadius: 15,
               tabBackgroundColor: const Color.fromRGBO(190, 231, 237, 1),
               iconSize: 16,
               curve: Curves.easeInCubic,
               color: Colors.grey,
-              tabs:  <GButton>[
+              tabs: <GButton>[
                 GButton(
                   icon: Icons.home,
                   text: "Home",
@@ -86,5 +106,36 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
+  }
+
+  void createDatabase() async {
+    database = await openDatabase("memories.db", version: 1,
+        onCreate: (database, version) {
+      print("database created!");
+      database
+          .execute(
+              "CREATE TABLE memory (id INTEGER PRIMARY KEY,title TEXT,descreption TEXT,img TEXT,date TEXT,time TEXT,status TEXT)")
+          .then((value) {
+        print("tablr created!");
+      }).catchError((error) {
+        print("Error whem creating table ${error.toString()}");
+      });
+    }, onOpen: (database) {
+      print("database Opened!");
+    });
+  }
+
+   void insertToDatabase() {
+    database.transaction((txn) {
+      txn
+          .rawInsert(
+              "INSERT INTO memory(title,descreption,img,date,time,status)  VALUES('first','desc','kjk','123','22','fav')")
+          .then((value) {
+        print("$value inserted successfully!");
+      }).catchError((error) {
+        print("Error when inserting new record $error");
+      });
+      return null;
+    });
   }
 }
