8adcdb0c911441306f17c07a5d5c10754e2a9d4f
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-12-30 09:31:13 +0200

    add ConditionalBuilder for no data

--- lib/views/memories_screen.dart
+++ lib/views/memories_screen.dart
@@ -1,5 +1,7 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:memory_bank/cubit/app_cubit/app_cubit.dart';
 import 'package:memory_bank/cubit/app_cubit/app_state.dart';
 import 'package:memory_bank/database/db_helper.dart';
@@ -16,21 +18,37 @@ class MemoriesScreen extends StatelessWidget {
       },
       builder: (context, state) {
         AppCubit cubit = AppCubit.get(context);
-        return GridView.builder(
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 210,
-            childAspectRatio: 0.6,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 1,
-          ),
-          physics: const BouncingScrollPhysics(),
-          itemCount: cubit.memories.length,
-          itemBuilder: (ctx, index) {
-            return MemoryItem(
-              model: cubit.memories[index],
-            );
-          },
+        return ConditionalBuilder(
+            condition: cubit.memories.isNotEmpty,
+            builder: (context){
+              return GridView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 210,
+                  childAspectRatio: 0.6,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 1,
+                ),
+                physics: const BouncingScrollPhysics(),
+                itemCount: cubit.memories.length,
+                itemBuilder: (ctx, index) {
+                  return MemoryItem(
+                    model: cubit.memories[index],
+                  );
+                },
+              );
+            },
+            fallback: (context){
+              return  Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.image_outlined,color: Colors.grey,size: 64,),
+                    Text("No Memories Yet,Start Add",style: GoogleFonts.aDLaMDisplay(fontSize: 16,color: Colors.grey),)
+                  ],
+                ),
+              );
+            },
         );
       },
     );
